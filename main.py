ebaec0aed19e46cc58bb0fd551c89dcd557c3c7a
Ivan Leguizamon <ivanleguizamon939@gmail.com>
2025-02-23 15:44:18 -0300

    Despligue

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ from pydantic import BaseModel
 from bd.database import engine, Base
 from routers.movie import routerMovie #Importo el objeto de mi movie.py de mi carpeta routers.
 from routers.users import login_user 
+import os
 
 app = FastAPI( #Es el nombre del objeto FastAPI.
         title = 'Aprendiendo FastApi',
@@ -47,6 +48,10 @@ Base.metadata.create_all(bind = engine)
 @app.get('/', tags = ['Inicio']) 
 def read_root():
     return HTMLResponse('<h2> Hola mundo!</h2>')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host = "0.0.0", port = port)
 
 
 
